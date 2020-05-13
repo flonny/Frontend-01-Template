9b5e5e2424bc283b3e79ffbf9b5442817b8f3ecd
范跃东 <632296950@qq.com>
2020-05-14 00:21:45 +0800

    parser

--- week05/toy-browser/client.js
+++ week05/toy-browser/client.js
@@ -27,49 +27,88 @@ class Request {
   }
   send(connection) {
     return new Promise((resolve, reject) => {
+      const parser = new ResponseParser();
       if (connection) {
         connection.write(this.toString());
       } else {
-        connection = net.createConnection({
-          host:this.host,
-          port: this.port },() => {
-            connection.write(req.toString())
-          })
+        connection = net.createConnection(
+          {
+            host: this.host,
+            port: this.port,
+          },
+          () => {
+            connection.write(this.toString());
+          }
+        );
       }
-      connection.on('data', (data) => {
-        resolve(`${data.toString()}`)
+      connection.on("data", (data) => {
+        // new Response(data);
+        parser.receive(data.toString());
+        console.log(parser.statusLine)
+        // resolve(`${data.toString()}`);
+        // console.log(parser.statusLine)
         connection.end();
       });
-      
-      connection.on('error', (error) => {
-        reject(error)
+
+      connection.on("error", (error) => {
+        reject(error);
         connection.end();
       });
-      
-      connection.on('end', () => {
-        console.log('disconnected from server');
+
+      connection.on("end", () => {
+        console.log("disconnected from server");
       });
-    })
- 
+    });
   }
 }
-let req = new Request({
-  methods: "POST",
-  host: "127.0.0.1",
-  port: "8088",
-  headers: {
-    'x-http': 'test'
-  },
-  body: {
-    name: "fan",
-  },
-});
 
-void async function () {
-  let response =  await  req.send()
-  console.log(response)
-}();
+
 class Response {}
-class ResponseParser {
 
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_VALUE = 3;
+    this.WAITING_HEADER_LINE_END = 4;
+    this.WAITING_HEADER_BLOCK_END = 5;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = "";
+    this.headers = {};
+    this.headerName = "";
+    this.headerValue = "";
+  }
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === "\r") {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    }
+  }
 }
+class TrunkedBodyparser {}
+
+void (async function () {
+  let req = new Request({
+    methods: "POST",
+    host: "127.0.0.1",
+    port: "8088",
+    headers: {
+      "x-http": "test",
+    },
+    body: {
+      name: "fan",
+    },
+  });
+  let response = await req.send();
+  console.log(response);
+})();
