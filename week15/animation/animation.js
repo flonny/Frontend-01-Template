1a86430ae38d31530f42381371afec4e494bd579
fanyuedong <632296950@qq.com>
2020-07-23 11:00:56 +0800

    animation

--- week15/animation/animation.js
+++ week15/animation/animation.js
@@ -20,29 +20,76 @@
  */
 class Timeline {
     constructor() {
+        this.state = 'inited'
+        this.requestID = null
+        this.pauseTime = null
         this.animations = []
     }
     tick() {
         let t = Date.now() - this.startTime
+        let activeAnimations = this.animations.filter(animation => !animation.finished)
+        for (let animation of activeAnimations) {
+            let { object, property, template, start, end, duration, delay, timingFunction,startTime } = animation
 
-        for (let animation of this.animations) {
-            let { object, property, template, start, end,duration, delay, timingFunction } = animation
-            if (t > animation.duration + animation.delay)
-                continue
-    
-            let progression = timingFunction((t - delay)/duration);
+            let progression = timingFunction((t - delay-startTime) / duration);
+            if (t > duration + delay) {
+                animation.finished = true
+                progression = 1
+            }
             let value = start + progression * (end - start)
             object[property] = template(value)
         }
-        requestAnimationFrame(() => this.tick())
+        if (activeAnimations.length) { this.requestID = requestAnimationFrame(() => this.tick()) }
+
+
     }
     start() {
+        if (this.state != 'inited')
+            return
+        this.state = "playing"
         this.startTime = Date.now()
         this.tick()
     }
-    add(animation) {
-        console.log(animation)
+    pause() {
+        if (this.state != 'playing')
+            return
+        this.state = "paused"
+        this.pauseTime = Date.now()
+        if (this.requestID !== null)
+            cancelAnimationFrame(this.requestID)
+    }
+    resume() {
+        if (this.state != 'paused')
+            return
+        this.state = "playing"
+        this.startTime += Date.now() - this.pauseTime
+        this.tick()
+    }
+
+    restart() {
+        if (this.state == "playing") {
+            this.pause();
+        }
+        for(let animation of this.animations) {
+            animation.finished = false
+        }
+        // = []
+        this.requestID = null
+        this.pauseTime = null
+        this.state = "playing"
+        this.startTime = Date.now()
+        this.tick()
+    }
+    add(animation, startTime) {
+        animation.finished = false
+
+        if (this.state == "playing") {
+            animation.startTime = startTime !== void 0?startTime: Date.now()-startTime
+        } else {
+            animation.startTime = startTime !== void 0?startTime: 0
+        }
         this.animations.push(animation)
+
     }
 }
 class Animation {
