0bf7b14764f854166f59fa0e125240ee1fc0d739
fanyuedong <632296950@qq.com>
2020-08-06 17:56:22 +0800

    update

--- week16/Panel/index.js
+++ week16/Panel/index.js
@@ -1,6 +1,6 @@
-class Panel {
-  constructor() {
-    this.root = document.createElement('div')
+class Component {
+  constructor(type) {
+    this.root = document.createElement(type)
     this.attributes = new Map()
     this.children = new Set()
   }
@@ -13,42 +13,67 @@ class Panel {
   mountTo(parent) {
     parent.appendChild(this.root)
   }
+
+}
+class Panel extends Component {
+  constructor() {
+    super()
+    this.root = document.createElement('div')
+  }
   render() {
     return <div>
-      <h1>{this.title}</h1>
+      <h1>42342
+
+      asdfas
+      </h1>
       <div>
-        {this.children}
+        34234234
       </div>
     </div>
   }
 }
-function create(type, attributes, children) {
-  let element
-  if (typeof type === 'function') {
-    element = new type
-    if (attributes) {
-      for (let prop in attributes) {
-        element.setAttribute(prop, attributes[prop])
-      }
-    }
-
-    if (children) {
-      for (let child of children) {
-        element.appendChild(child)
-      }
-    }
-
-  }
-  return element
+function isLetter(c) {
+  const index = c.charCodeAt(0)
+  return (index >= 97 && index <= 122) || (index >= 65 && index <= 90)
 }
-function render(component,root) {
-  console.log(component)
+function typeToString(data) {
+  let type = Object.prototype.toString.call(data)
+  let stringType = ''
+  const waitSpace = 'WAIT_SPACE'
+  const waitChar = 'WAIT_Char'
+  let state = waitSpace
+  for (let c of type) {
+    if (state === waitSpace && c === ' ') {
+      state = waitChar
+    } else if (state === waitChar && isLetter(c)) {
+      stringType += c
+    }
+  }
+  return stringType.toLowerCase()
+}
+
+function create(type, attributes, ...children) {
+  let component
+  if (typeof type === 'string') {
+    component = new Component(type)
+  } else {
+    component = new type
+  }
+  if(typeToString(children) === 'array') {
+  console.log(children)
+  }
+  return component
+}
+function render(component, root) {
+  // console.log(component.render())
   component.mountTo(root)
 }
 let panel = <Panel>
-
+  <div></div>
+  <p></p>
 </Panel>
 render(
   panel,
   document.getElementById('root')
 );
+console.log(typeToString(1231))
